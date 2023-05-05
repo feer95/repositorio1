b79f221b0d2aa0fab82b76f1ab290d968af904dd
Feer95 <fernandofs.ffs@gmail.com>
2023-05-05 12:27:49 +0200

    reto

---
+++ reto.js
@@ -0,0 +1,25 @@
+function sum(x, y) {
+    let resultado
+    resultado = x + y
+    return resultado
+}
+function res(x, y) {
+    let resultado
+    resultado = x - y
+    return resultado
+}
+function div(x, y) {
+    let resultado
+    resultado = x / y
+    return resultado
+}
+function mul(x, y) {
+    let resultado
+    resultado = x * y
+    return resultado
+}
+function cuadrado(x) {
+    let resultado
+    resultado = x * x
+    return resultado
+}
